36986225ce1fca3db87ee514838da4fcba60b64a
GamingStudios21 <56083422+soham2121@users.noreply.github.com>
2021-10-02 19:05:09 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,17 +2,18 @@ import firebase from 'firebase'
 
 require('@firebase/firestore');
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBSkwUzi04uq3oWwWAorzVJxUAa0fiS3HA",
   authDomain: "book-santa123.firebaseapp.com",
+  databaseURL: "https://book-santa123-default-rtdb.firebaseio.com",
   projectId: "book-santa123",
   storageBucket: "book-santa123.appspot.com",
   messagingSenderId: "1061122479524",
-  appId: "1:1061122479524:web:a0618bbffe578c698f9d31"
+  appId: "1:1061122479524:web:70d94bcbecaaa2bf8f9d31"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default firebase.firestore();
